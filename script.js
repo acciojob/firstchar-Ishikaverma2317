fbca127d509b466acda5895ec9c8b7eddb63865f
Ishika Verma <ishikaverma2317@gmail.com>
2025-08-18 03:48:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
-  // your code here
+  // Remove leading spaces
+  text = text.trimStart();
+
+  // Agar string khali ho gayi to return empty
+  if (text.length === 0) {
+    return "";
+  }
+
+  // Otherwise first character return
+  return text[0];
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");  // <-- isko uncomment rakho
 alert(firstChar(text));
